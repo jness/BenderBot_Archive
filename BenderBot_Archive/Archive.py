9d7026d4420f3a389976f22c6ff0a1391487237f
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-06-30 11:57:01 -0500

    working on text file func

--- BenderBot_Archive/Archive.py
+++ BenderBot_Archive/Archive.py
@@ -3,6 +3,7 @@ from BenderBot_Responder.Requests import Requests
 from time import sleep
 from re import match
 from datetime import datetime
+import os
 
 class Archive(BenderProcess):
     
@@ -20,12 +21,18 @@ class Archive(BenderProcess):
                             (datetime.now().ctime(), nick, message)
         return self.f_msg
     
+    def __appendLog(self):
+        d = datetime.now()
+        filename = '%s-%s-%s.txt' % (d.year, d.month, d.day)
+        f = open(filename, 'a+')
+        f.write(self.f_msg + '\n')
+        f.close()
+    
     def run(self):
         while True:
             if self.__getMessage():
                 if self.__formatMessage():
-                    # need a function that writes to text file here,
-                    # we can then use that in a web interface.
-                    print self.f_msg
+                    self.logger.debug('Appending message to text file')
+                    self.__appendLog()
                         
             sleep(0.02) # Slow down the loop just a bit to avoid CPU melt ;)
